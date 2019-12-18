f7498b1df0eec233bf5aef132d6d9933fa38e8b0
Vittozich <vittozich@gmail.com>
2019-12-18 16:44:02 +0300

    creating project like as in the Video

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-# Create your views here.
+from .models import Question, Choice
+
+# get questions and display. This is actually controller
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    contest = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', contest)
+
+# show question and choices
+
+def detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Вопроса не существует")
+  return render(request, 'polls/detail.html', {'question': question})
+
+# display results
+
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question': question})
+
+  # Vote for a question choice
+def vote(request, question_id):
+    # print(request.POST['choice'])
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
